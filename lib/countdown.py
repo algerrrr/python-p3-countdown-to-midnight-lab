b3d30596003e8fc8127cc37eff43a267af6e7bcc
Algernon Soriano <alger.soriano@gmail.com>
2023-11-15 19:53:31 +1000

    countdown lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
-# your code goes here!
+import time
+
+def countdown(num):
+    x = num
+    while x > 0:
+        print(f'{x} SECOND(S)!')
+        x-= 1
+    print("HAPPY NEW YEAR!")
+
+#countdown(10)
+
+def countdown_with_sleep(num):
+    x = num
+    while x > 0:
+        print(f'{x} SECOND(S)!')
+        x-= 1
+        time.sleep(2)
+    print("HAPPY NEW YEAR!")
